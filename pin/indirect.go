1673b9cc3c3aa697ff970fc90581b610028da915
Tommi Virtanen <tv@eagain.net>
2015-05-11 11:19:36 -0700

    pin: Rewrite to store pins in IPFS objects
    WARNING: No migration performed! That needs to come in a separate
    commit, perhaps amended into this one.
    
    This is the minimal rewrite, only changing the storage from
    JSON(+extra keys) in Datastore to IPFS objects. All of the pinning
    state is still loaded in memory, and written from scratch on Flush. To
    do more would require API changes, e.g. adding error returns.
    
    Set/Multiset is not cleanly separated into a library, yet, as it's API
    is expected to change radically.

--- pin/indirect.go
+++ pin/indirect.go
@@ -1,9 +1,6 @@
 package pin
 
-import (
-	ds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/jbenet/go-datastore"
-	"github.com/ipfs/go-ipfs/util"
-)
+import "github.com/ipfs/go-ipfs/util"
 
 type indirectPin struct {
 	refCounts map[util.Key]uint64
@@ -13,36 +10,6 @@ func newIndirectPin() *indirectPin {
 	return &indirectPin{
 		refCounts: make(map[util.Key]uint64),
 	}
-}
-
-func loadIndirPin(d ds.Datastore, k ds.Key) (*indirectPin, error) {
-	var rcStore map[string]uint64
-	err := loadSet(d, k, &rcStore)
-	if err != nil {
-		return nil, err
-	}
-
-	refcnt := make(map[util.Key]uint64)
-	var keys []util.Key
-	for encK, v := range rcStore {
-		if v > 0 {
-			k := util.B58KeyDecode(encK)
-			keys = append(keys, k)
-			refcnt[k] = v
-		}
-	}
-	// log.Debugf("indirPin keys: %#v", keys)
-
-	return &indirectPin{refCounts: refcnt}, nil
-}
-
-func storeIndirPin(d ds.Datastore, k ds.Key, p *indirectPin) error {
-
-	rcStore := map[string]uint64{}
-	for k, v := range p.refCounts {
-		rcStore[util.B58KeyEncode(k)] = v
-	}
-	return storeSet(d, k, rcStore)
 }
 
 func (i *indirectPin) Increment(k util.Key) {
